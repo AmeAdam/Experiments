b0ea66a368ba263aa084ff18ecfdc16a5a31d05d
AmeAdam <adam.baszynski@gmail.com>
2016-01-30 23:05:37 +0100

    SvnDirectoryWatcher

---
+++ AmeWindowsService/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmeWindowsService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            Settings.Default["Setting1"] = "abc";
+            Settings.Default.Save();
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ AmeWindowsService/SvnAuto/SvnDirectoryWatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmeWindowsService.SvnAuto
+{
+    class SvnDirectoryWatcher
+    {
+        FileSystemWatcher watcher;
+
+        public SvnDirectoryWatcher()
+        {
+            watcher = new FileSystemWatcher("e:\\projekty");
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.IncludeSubdirectories = true;
+            watcher.Filter = "*.prproj";
+
+        }
+    }
+}
